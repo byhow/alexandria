660b975831c1b97a7b325533cfb82457ef8ba93a
byhow <by.how@hotmail.com>
2023-07-25 12:55:02 -0700

    feat: add color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import { SketchPicker } from 'react-color';
+import { useState } from "react";
+import { SketchPicker } from "react-color";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
+  const [color, setColor] = useState("#1f47f9");
+  const [gradient, setGradient] = useState([]);
   return (
     <div>
-      <SketchPicker />
+      <SketchPicker
+        color={color}
+        onChange={(color) => {
+          setColor(color.hex);
+          invoke(
+            "generate_gradient",
+            color.rgb as unknown as Record<string, unknown> // will figure this out later
+          ).then((grad) => {
+            console.log(grad);
+            setGradient(grad as []);
+          });
+        }}
+      />
+      {gradient.map((color) => (
+        <div
+          style={{
+            padding: "2rem",
+            background: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+          }}
+        >
+          rgb({color[0]}, {color[1]}, {color[2]})
+        </div>
+      ))}
     </div>
   );
 }
